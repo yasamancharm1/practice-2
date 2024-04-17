5f099db372e9b04f32336fcedef364e0801934c6
yasamancharm1 <166140747+yasamancharm1@users.noreply.github.com>
2024-04-17 19:08:26 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c
+    cout << "please enter a." ;
+    cin >> a ;
+    cout << "please enter b."
+    cin >> b ;
+    cout << "please enter C."
+    cin >> c ;
+    cout << "a=" << a << endl;
+    cout << "b=" << b << endl;
+    cout << "c=" << c <<endl;
+    return 0;
+}
